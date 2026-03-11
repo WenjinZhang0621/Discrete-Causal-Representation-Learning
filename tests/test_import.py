656368f4938e59bcb82d016d39179c6c86453143
Wenjin Zhang <113813432+WenjinZhang0621@users.noreply.github.com>
2026-03-10 22:32:48 -0400

    Reorder import statement for pathlib

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import sys
+from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[1]
 SRC = ROOT / "src"
@@ -11,3 +11,4 @@ import dcrl
 
 def test_import():
     assert dcrl is not None
+
