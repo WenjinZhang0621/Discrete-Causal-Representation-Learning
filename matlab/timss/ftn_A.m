58e76d1b963b0ffa6d3c067fcfcf83a6c39c296d
Wenjin Zhang <113813432+WenjinZhang0621@users.noreply.github.com>
2026-03-15 19:42:31 -0400

    Delete matlab/timss directory

--- matlab/timss/ftn_A.m
+++
@@ -1,8 +0,0 @@
-function C = ftn_A(eta)
-% lognormal normalizing constant A
-     %C=exp(eta);
-   %eta_1 = max(eta(:,1),0.1);
-   eta_1=eta(:,1);
-    eta_2 = eta(:,2);
-    C = eta_2 .^2 ./ (4 * eta_1) + log( 1 ./ (2 * eta_1)) /2;
-end
--- matlab/timss/ftn_T.m
+++
@@ -1,8 +0,0 @@
-function T = ftn_T(X)
-% lognormal sufficient statistics T
-
-    %T=X;
-    T_1 =  -(log(X)).^2;
-    T_2 = log(X);
-    T = [T_1; T_2];
-end
